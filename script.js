14d9fce2d2e6e8d1ddf713d7007e9de39da73928
Andres Lora <jalc.andres.lora@gmail.com>
2023-05-27 13:10:01 -0700

    replaced property hidden

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide loading
+function hiddenLoading() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 // Show New Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if the author field is blank and replace it with 'Unknown'
@@ -21,10 +33,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  // Show quote
   quoteText.textContent = quote.text;
+  // Hide loading
+  hiddenLoading();
 }
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
